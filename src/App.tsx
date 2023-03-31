ca7272ce3097b84f306cc582778ea78d446eac7b
Deni <dv04111993@yandex.ru>
2023-03-31 12:43:33 +0200

    html css

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,37 @@ import "./index.css"
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <div className="app">
-      <h1 className="title">WE'RE LAUNCHING SOON</h1>
-      <div className="timer"></div>
+    <div>
+      <div className="content-wrapper">
+        <div className="title">
+          <p>WE'RE LAUNCHING SOON</p>
+        </div>
+        <div className="counter">
+          <div className="time days">
+            <div className="box">23</div>
+            <p>DAYS</p>
+          </div>
+          <div className="time hours">
+            <div className="box">23</div>
+            <p>HOURS</p>
+          </div>
+          <div className="time minutes">
+            <div className="box">23</div>
+            <p>MINUTES</p>
+          </div>
+          <div className="time seconds">
+            <div className="box">23</div>
+            <p>SECONDS</p>
+          </div>
+        </div>
+      </div>
+
       <footer>
-        <h1>icons</h1>
+        <div className="icons">
+          <img src="/images/icon-facebook.svg" alt="a" />
+          <img src="/images/icon-pinterest.svg" alt="b" />
+          <img src="/images/icon-instagram.svg" alt="c" />
+        </div>
       </footer>
     </div>
   )
